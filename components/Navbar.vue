00ec2c31a903d29960c8d1893c3faf6aec51921c
Kankawee Aramrak <kankaweeisci123@gmail.com>
2024-03-17 11:46:15 +0700

    (UI): Add Page1

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,40 +1,40 @@
 <template>
-    <div class="w-screen h-[60px] fixed z-40" @click="hamberBTN('screen')" @mousedown.prevent="handleMouseDown" @mouseup="handleMouseUp"
+    <div class="w-screen fixed z-40" @click="hamberBTN('screen')" @mousedown.prevent="handleMouseDown" @mouseup="handleMouseUp"
         @touchstart="handleTouchStart" @touchend="handleTouchEnd">
-        <div class="flex justify-between items-center h-[60px] px-[15px]">
+        <div class="flex justify-between items-center h-[6.5vh] px-[15px]">
             <NuxtLink>
-                <h4 class="text-white text-[16px] font-bold">Nanami Bot</h4>
+                <h4 class="text-[18px] font-bold transition-colors duration-200" :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">Nanami Bot</h4>
             </NuxtLink>
 
             <button class="lg:hidden grid gap-1 mt-0 bg-transparent p-2 select-none" @click="hamberBTN('ham')">
-                <span id="ham1" :class="{ 'opacity-0': istoggle == true }"
-                    class="w-6 h-1 bg-white rounded-[10px] transition-all duration-200"></span>
-                <span id="ham2" :class="{ 'opacity-0': istoggle == true }"
-                    class="w-6 h-1 bg-white rounded-[10px] transition-all duration-200"></span>
-                <span id="ham3" :class="{ 'opacity-0': istoggle == true }"
-                    class="w-6 h-1 bg-white rounded-[10px] transition-all duration-200"></span>
+                <span id="ham1" :class="{ 'opacity-0': istoggle == true, 'bg-[#02071A]': !pagesTheme, 'bg-white': pagesTheme }"
+                    class="w-6 h-1 rounded-[10px] transition-all duration-200"></span>
+                <span id="ham2" :class="{ 'opacity-0': istoggle == true, 'bg-[#02071A]': !pagesTheme, 'bg-white': pagesTheme }"
+                    class="w-6 h-1 rounded-[10px] transition-all duration-200"></span>
+                <span id="ham3" :class="{ 'opacity-0': istoggle == true, 'bg-[#02071A]': !pagesTheme, 'bg-white': pagesTheme }"
+                    class="w-6 h-1 rounded-[10px] transition-all duration-200"></span>
             </button>
         </div>
         <div class="w-sceeen flex justify-end fixed right-0 top-0 w-screen transition-all duration-200"
             :class="{ 'translate-x-[0]': istoggle, 'translate-x-[100vw]': !istoggle }">
             <div class="lg:hidden space-y-[30px] bg-gradient-to-l from-white/5 from-[-20%] to-transparent h-screen rounded-l-[20px] text-end pr-[50px] pt-[100px] transition-all duration-200 overflow-hidden"
                 :class="{ 'w-0': !istoggle, 'w-[60%]': istoggle }">
-                <div class="text-white transition-opacity duration-[500ms]" :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle }">
+                <div class="text-white font-bold text-[16px] transition-opacity duration-[500ms]" :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
                     <NuxtLink @click="hamberBTN('menu')" to="/">
                         <h6><i class="fas fa-home-alt mr-2"></i>Home</h6>
                     </NuxtLink>
                 </div>
-                <div class="text-white transition-opacity duration-[700ms]" :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle }">
+                <div class="text-white font-bold text-[16px] transition-opacity duration-[700ms]" :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
                     <NuxtLink @click="hamberBTN('menu')" to="/">
                         <h6><i class="far fa-dot-circle mr-2"></i>Support</h6>
                     </NuxtLink>
                 </div>
-                <div class="text-white transition-opacity duration-[1100ms]" :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle }">
+                <div class="text-white font-bold text-[16px] transition-opacity duration-[1100ms]" :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
                     <NuxtLink @click="hamberBTN('menu')" to="/">
                         <h6><i class="fad fa-hands-helping mr-2"></i>Partners</h6>
                     </NuxtLink>
                 </div>
-                <div class="text-white transition-opacity duration-[900ms]" :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle }">
+                <div class="text-white font-bold text-[16px] transition-opacity duration-[900ms]" :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
                     <NuxtLink @click="hamberBTN('menu')" to="/">
                         <h6><i class="far fa-code mr-2"></i>Developers</h6>
                     </NuxtLink>
@@ -44,19 +44,23 @@
     </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, defineEmits, onMounted, onUnmounted } from 'vue';
 
 const istoggle = ref(false);
+let pagesTheme = ref(true);
 let startX: number | null = null; // Store the starting position
-
+const emit = defineEmits(['update:istoggle']);
 
 let menuOpening = false;
 function hamberBTN(from: string) {
     //ถ้าเมนูกำลังเปิด แต่มีคำสั่งปิดให้เปิดต่อไป
-    if (menuOpening) return istoggle.value = true;
+    if (menuOpening) {
+        emit('update:istoggle', true);
+        return istoggle.value = true;
+    }
     if (istoggle.value === false && from === 'screen') return;
     istoggle.value = !istoggle.value;
-    console.log(istoggle.value);
+    emit('update:istoggle', istoggle.value);
     menuOpening = true;
     setTimeout(() => {
         menuOpening = false;
@@ -76,6 +80,7 @@ function handleMouseUp(event: MouseEvent) {
     const distance = endX - startX;
     if (distance > 50) { // Slide to the right detected
         istoggle.value = false;
+        emit('update:istoggle', false);
     }
     startX = null; // Reset for the next action
 }
@@ -94,7 +99,21 @@ function handleTouchEnd(event: TouchEvent) {
     const distance = endX - startX;
     if (distance > 50) { // Slide to the right detected
         istoggle.value = false;
+        emit('update:istoggle', false);
     }
     startX = null; // Reset for the next action
 }
+
+function handleScroll(): void {
+      const targetHeight = 825; // Set to desired scroll height
+      pagesTheme.value = window.scrollY <= targetHeight;
+}
+
+onMounted(() => {
+    window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('scroll', handleScroll);
+});
 </script>
