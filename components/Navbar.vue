c911ed55708b71e54320117d77388d6d0967b6f4
Kankawee Aramrak <kankaweeisci123@gmail.com>
2024-04-10 02:04:14 +0700

    (UI): add color to navbar when stay at that page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,7 +2,7 @@
     <div class="w-screen fixed z-40" @click="hamberBTN('screen')" @mousedown.prevent="handleMouseDown"
         @mouseup="handleMouseUp" @touchstart="handleTouchStart" @touchend="handleTouchEnd">
         <div class="flex justify-between items-center h-[6.5vh] px-[15px] lg:px-[25px] xl:my-[20px]">
-            <NuxtLink>
+            <NuxtLink to="/">
                 <h4 class="text-[18px] font-bold transition-colors duration-200 lg:text-[25px] 2xl:text-[35px]"
                     :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">Nanami Bot</h4>
             </NuxtLink>
@@ -23,31 +23,31 @@
             <!-- Desktop Menu -->
             <div class="hidden h-full items-center lg:flex lg:justify-end lg:space-x-[20px] 2xl:space-x-[30px]">
                 <div class="font-bold text-[18px] transition-all duration-200 2xl:text-[25px]"
-                    :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
+                    :class="{ 'text-[#02071A]': !pagesTheme && path != '/', 'text-white': pagesTheme && path != '/', 'text-[#0099FF]': path == '/' }">
                     <NuxtLink to="/">
                         <h4>Home</h4>
                     </NuxtLink>
                 </div>
                 <div class="font-bold text-[18px] transition-all duration-200 2xl:text-[25px]"
-                    :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
+                    :class="{ 'text-[#02071A]': !pagesTheme && path != '/support', 'text-white': pagesTheme && path != '/support', 'text-[#0099FF]': path == '/support' }">
                     <NuxtLink to="/support">
                         <h4>Support</h4>
                     </NuxtLink>
                 </div>
                 <div class="font-bold text-[18px] transition-all duration-200 2xl:text-[25px]"
-                    :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
+                    :class="{ 'text-[#02071A]': !pagesTheme && path != '/partners', 'text-white': pagesTheme && path != '/partners', 'text-[#0099FF]': path == '/partners' }">
                     <NuxtLink to="/partners">
                         <h4>Partners</h4>
                     </NuxtLink>
                 </div>
                 <div class="font-bold text-[18px] transition-all duration-200 2xl:text-[25px]"
-                    :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
+                    :class="{ 'text-[#02071A]': !pagesTheme && path != '/developers', 'text-white': pagesTheme && path != '/developers', 'text-[#0099FF]': path == '/developers' }">
                     <NuxtLink to="/developers">
                         <h4>Developers</h4>
                     </NuxtLink>
                 </div>
                 <div class="font-bold text-[18px] transition-all duration-200 2xl:text-[25px]"
-                    :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
+                    :class="{ 'text-[#02071A]': !pagesTheme && path != '/commands', 'text-white': pagesTheme && path != '/commands', 'text-[#0099FF]': path == '/commands' }">
                     <NuxtLink to="/commands">
                         <h4>Commands</h4>
                     </NuxtLink>
@@ -71,31 +71,31 @@
             <div class="space-y-[30px] bg-gradient-to-l from-[-20%] to-transparent h-screen rounded-l-[20px] text-end pr-[50px] pt-[100px] transition-all duration-200 overflow-hidden"
                 :class="{ 'w-0': !istoggle, 'w-[60%]': istoggle, 'from-white/5': pagesTheme, 'from-black/20': !pagesTheme }">
                 <div class="font-bold text-[16px] transition-all duration-[500ms]"
-                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
+                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme && path != '/', 'text-white': pagesTheme && path != '/', 'text-[#0099FF]': path == '/'}">
                     <NuxtLink @click="hamberBTN('menu')" to="/">
                         <h6><i class="fas fa-home-alt mr-2"></i>Home</h6>
                     </NuxtLink>
                 </div>
                 <div class="font-bold text-[16px] transition-all duration-[700ms]"
-                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
+                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme && path != '/support', 'text-white': pagesTheme && path != '/support', 'text-[#0099FF]': path == '/support' }">
                     <NuxtLink @click="hamberBTN('menu')" to="/support">
                         <h6><i class="far fa-dot-circle mr-2"></i>Support</h6>
                     </NuxtLink>
                 </div>
                 <div class="font-bold text-[16px] transition-all duration-[900ms]"
-                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
+                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme && path != '/partners', 'text-white': pagesTheme && path != '/partners', 'text-[#0099FF]': path == '/partners' }">
                     <NuxtLink @click="hamberBTN('menu')" to="/partners">
                         <h6><i class="fad fa-hands-helping mr-2"></i>Partners</h6>
                     </NuxtLink>
                 </div>
                 <div class="font-bold text-[16px] transition-all duration-[1100ms]"
-                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
+                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme && path != '/developers', 'text-white': pagesTheme && path != '/developers', 'text-[#0099FF]': path == '/developers' }">
                     <NuxtLink @click="hamberBTN('menu')" to="/developers">
                         <h6><i class="far fa-code mr-2"></i>Developers</h6>
                     </NuxtLink>
                 </div>
                 <div class="font-bold text-[16px] transition-all duration-[1300ms]"
-                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }">
+                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme && path != '/commands', 'text-white': pagesTheme && path != '/commands', 'text-[#0099FF]': path == '/commands' }">
                     <NuxtLink @click="hamberBTN('menu')" to="/commands">
                         <h6><i class="far fa-terminal mr-2"></i>Commands</h6>
                     </NuxtLink>
@@ -112,6 +112,7 @@ let pagesTheme = ref();
 let startX: number | null = null; // Store the starting position
 const emit = defineEmits(['update:istoggle']);
 const props = defineProps(['theme', 'scroll_value']);
+const path = ref(useRoute().path);
 
 //PageTheme Update
 setInterval(() => {
