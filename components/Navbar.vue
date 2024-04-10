153c8996426efb7dc39b9ecc4e6f19884e3583b0
Kankawee Aramrak <kankaweeisci123@gmail.com>
2024-04-10 16:01:27 +0700

    (UI): Add submenu to Navbar and Signin

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -53,12 +53,16 @@
                     </NuxtLink>
                 </div>
                 <div class="space-x-[10px] h-full flex items-center">
-                    <div class="w-[3px] h-[30px]" :class="{ 'bg-[#02071A]': !pagesTheme, 'bg-white': pagesTheme }"></div>
+                    <div class="w-[3px] h-[30px]" :class="{ 'bg-[#02071A]': !pagesTheme, 'bg-white': pagesTheme }">
+                    </div>
 
-                    <NuxtLink class="fab fa-github text-[27px]" :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }"></NuxtLink>
-                    <NuxtLink class="fab fa-discord text-[27px]" :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }"></NuxtLink>
+                    <NuxtLink class="fab fa-github text-[27px]"
+                        :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }"></NuxtLink>
+                    <NuxtLink class="fab fa-discord text-[27px]"
+                        :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }"></NuxtLink>
 
-                    <div class="w-[3px] h-[30px] " :class="{ 'bg-[#02071A]': !pagesTheme, 'bg-white': pagesTheme }"></div>
+                    <div class="w-[3px] h-[30px] " :class="{ 'bg-[#02071A]': !pagesTheme, 'bg-white': pagesTheme }">
+                    </div>
                 </div>
                 <button class="fad fa-user text-[21px] 2xl:text-[30px]"
                     :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }"></button>
@@ -68,10 +72,10 @@
         <!-- Mobile Menu -->
         <div class="w-sceeen flex justify-end fixed right-0 top-0 w-screen transition-all duration-200 lg:hidden"
             :class="{ 'translate-x-[0]': istoggle, 'translate-x-[100vw]': !istoggle }">
-            <div class="space-y-[30px] bg-gradient-to-l from-[-20%] to-transparent h-screen rounded-l-[20px] text-end pr-[50px] pt-[100px] transition-all duration-200 overflow-hidden"
+            <div class="space-y-[30px] bg-gradient-to-l from-[-20%] to-transparent h-screen rounded-l-[20px] text-end pr-[50px] pt-[100px] transition-all duration-200 overflow-y-scroll overflow-x-hidden"
                 :class="{ 'w-0': !istoggle, 'w-[60%]': istoggle, 'from-white/5': pagesTheme, 'from-black/20': !pagesTheme }">
                 <div class="font-bold text-[16px] transition-all duration-[500ms]"
-                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme && path != '/', 'text-white': pagesTheme && path != '/', 'text-[#0099FF]': path == '/'}">
+                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme && path != '/', 'text-white': pagesTheme && path != '/', 'text-[#0099FF]': path == '/' }">
                     <NuxtLink @click="hamberBTN('menu')" to="/">
                         <h6><i class="fas fa-home-alt mr-2"></i>Home</h6>
                     </NuxtLink>
@@ -98,6 +102,30 @@
                     :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme && path != '/commands', 'text-white': pagesTheme && path != '/commands', 'text-[#0099FF]': path == '/commands' }">
                     <NuxtLink @click="hamberBTN('menu')" to="/commands">
                         <h6><i class="far fa-terminal mr-2"></i>Commands</h6>
+                    </NuxtLink>
+                </div>
+                <div class="w-full space-y-[10px]">
+                    <div class="w-full h-max flex justify-end">
+                        <div class="w-[60%] h-[3px] transition-all duration-[1500ms]"
+                            :class="{ 'bg-[#02071A]': !pagesTheme, 'bg-white': pagesTheme }"></div>
+                    </div>
+                    <div class="space-x-[10px] transition-all duration-[1500ms]">
+
+                        <NuxtLink class="fab fa-github text-[27px] transition-all duration-[1500ms]"
+                            :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }"></NuxtLink>
+                        <NuxtLink class="fab fa-discord text-[27px] transition-all duration-[1500ms]"
+                            :class="{ 'text-[#02071A]': !pagesTheme, 'text-white': pagesTheme }"></NuxtLink>
+                    </div>
+
+                    <div class="w-full h-max flex justify-end">
+                        <div class="w-[60%] h-[3px] transition-all duration-[1500ms]"
+                            :class="{ 'bg-[#02071A]': !pagesTheme, 'bg-white': pagesTheme }"></div>
+                    </div>
+                </div>
+                <div class="font-bold text-[16px] transition-all duration-[1300ms]"
+                    :class="{ 'opacity-100': istoggle, 'opacity-0': !istoggle, 'text-[#02071A]': !pagesTheme && path != '/signin', 'text-white': pagesTheme && path != '/signin', 'text-[#0099FF]': path == '/signin' }">
+                    <NuxtLink @click="hamberBTN('menu')" to="/signin">
+                        <h6><i class="fas fa-sign-in mr-2"></i>Sign In</h6>
                     </NuxtLink>
                 </div>
             </div>
